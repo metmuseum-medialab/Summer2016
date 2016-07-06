9f56e97941b43d8c5b097fc04f9c36bc856d67ea
alanoodalthani <anood.thani@gmail.com>
2016-07-06 15:58:07 -0400

    Create random.js

---
+++ IslamicArt/random.js
@@ -0,0 +1,20 @@
+<!-- hide script from old browsers
+var howMany = 3;  // max number of items listed below
+var page = new Array(howMany+1);
+
+page[0]="0001.html";
+page[1]="0002.html";	
+page[2]="0003.html";
+page[3]="0004.html";
+
+function rndnumber(){
+var randscript = -1;
+while (randscript < 0 || randscript > howMany || isNaN(randscript)){
+randscript = parseInt(Math.random()*(howMany+1));
+}
+return randscript;
+}
+quo = rndnumber();
+quox = page[quo];
+window.location=(quox);
+// end hiding script from old browsers -->

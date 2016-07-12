8dfe61a46346a46eb4492c68178d0326998baaa6
alanoodalthani <anood.thani@gmail.com>
2016-07-12 11:28:42 -0400

    Update random.js

--- IslamicArt/random.js
+++ IslamicArt/random.js
@@ -1,70 +1,42 @@
 <!-- hide script from old browsers
-
-var howMany = 17;  // max number of items listed below
-
+var howMany = 24;  // max number of items listed below
 var page = new Array(howMany+1);
 
-
-
-
 page[0]="0001.html";
-
 page[1]="0002.html";	
-
 page[2]="0003.html";
-
 page[3]="0004.html";
-
 page[4]="0005.html";
-
 page[5]="0006.html";
-
 page[6]="0007.html";
-
 page[7]="0008.html";
-
 page[8]="0009.html";
-
 page[9]="0010.html";
-
 page[10]="0011.html";
-
 page[11]="0012.html";
-
 page[12]="0013.html";
-
 page[13]="0014.html";
-
 page[14]="0015.html";
-
 page[15]="0016.html";
-
 page[16]="0017.html";
-
 page[17]="0018.html";
-
+page[18]="0019.html";
+page[19]="0020.html";
+page[20]="0021.html";
+page[21]="0022.html";
+page[22]="0023.html";
+page[23]="0024.html";
+page[24]="0025.html";
 
 
 function rndnumber(){
-
 var randscript = -1;
-
 while (randscript < 0 || randscript > howMany || isNaN(randscript)){
-
 randscript = parseInt(Math.random()*(howMany+1));
-
 }
-
 return randscript;
-
 }
-
 quo = rndnumber();
-
 quox = page[quo];
-
 window.location=(quox);
-
 // end hiding script from old browsers -->
-
-
